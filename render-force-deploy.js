ae4fcbc2d26c74669ceab2e25c1c643ab31d64b9
User <user@example.com>
2025-06-20 11:48:44 +0200

    Force clean build on Render with no cache

---
+++ render-force-deploy.js
@@ -0,0 +1,8 @@
+/**
+ * This file exists solely to force Render to perform a clean deployment
+ * by creating a new file that will be detected by Git.
+ * 
+ * Deployment timestamp: 2025-06-21 10:15:00
+ */
+
+console.log('Forcing clean deployment on Render...'); 
